eabbb62ac042355447f677ab387cf92730599aa9
Mikhail Gerasimov <mgerasim@inbox.ru>
2012-05-06 12:44:52 +0300

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geizer::Application.config.secret_token = 'e33e85cdff00276c3f219c22834d382e556b1873a7a8d154cfafd0b302e5e167f41c6b07522089ab1dfc56c66beceb80c28ffa41d71a8c4d6c1140be25ba34ad'
