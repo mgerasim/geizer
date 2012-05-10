47bc48381cc3eb8f3633e9e4564197be37a40600
Mikhail Gerasimov <mgerasim@inbox.ru>
2012-05-10 13:53:42 +0300

    S0001

--- config/routes.rb
+++ config/routes.rb
@@ -56,7 +56,7 @@ Geizer::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'Clients#index'
+  root :to => 'clients#index'
 
   # See how all your routes lay out with "rake routes"
 
